e80c708de1f3c48d808757e0b3c951b8927067e9
MCTTAN <mariechantaltan@gmail.com>
2019-08-06 02:32:42 +0000

    commit

--- ui/main.R
+++ ui/main.R
@@ -14,7 +14,7 @@ hidden(fluidRow(
     12,
     div(
       style = "padding: 50px",
-      h1("You and now signed in and your email address has been verified!"),
+      h1("You're now signed in and your email address has been verified!"),
       br(),
       br(),
       h3("This is all the information that comes with the signed in user with 'Email/Password' authentication on Firebase"),
