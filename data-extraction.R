0118ea38a98aed6b2f6d2d9a52c5568001f3a861
MCTTAN <mariechantaltan@gmail.com>
2019-08-19 12:15:46 -0400

    commit

--- data-extraction.R
+++ data-extraction.R
@@ -9,6 +9,8 @@ subtable <- subset(data_crime, city_name == "Austin")
 write.csv(MyData, file = "data-crime-austin.csv")
 # data <-read.csv("http://www.calvin.edu/~lk24/courses/Data/grandrapidsweather.csv")
 
+# https://developer.ibm.com/clouddataservices/2016/11/03/open-crime-data/
+
 # https://www.r-bloggers.com/getting-data-from-an-online-source/
 # https://theodi.org/article/how-to-use-r-to-access-data-on-the-web/
 # https://www.dezyre.com/data-science-in-r-programming-tutorial/r-tutorial-importing-data-from-web
