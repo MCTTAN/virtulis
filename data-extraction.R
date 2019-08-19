0a6926da2cdec39e6ca25f54f2cde98865a0f600
MCTTAN <mariechantaltan@gmail.com>
2019-08-19 12:13:17 -0400

    commit

--- data-extraction.R
+++ data-extraction.R
@@ -7,6 +7,9 @@ list_crime_data()
 subtable <- subset(data_crime, city_name == "Austin")
 
 write.csv(MyData, file = "data-crime-austin.csv")
+# data <-read.csv("http://www.calvin.edu/~lk24/courses/Data/grandrapidsweather.csv")
+
+
 # write.csv(MyData, file = "MyData.csv",row.names=FALSE)
 # write.csv(MyData, file = "MyData.csv",row.names=FALSE, na="")
 # write.table(MyData, file = "MyData.csv",row.names=FALSE, na="",col.names=FALSE, sep=",")
