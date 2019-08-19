c061bd0e0c27742d654c2765dcea72b235d190d4
MCTTAN <mariechantaltan@gmail.com>
2019-08-19 12:15:02 -0400

    commit

--- data-extraction.R
+++ data-extraction.R
@@ -10,6 +10,8 @@ write.csv(MyData, file = "data-crime-austin.csv")
 # data <-read.csv("http://www.calvin.edu/~lk24/courses/Data/grandrapidsweather.csv")
 
 # https://www.r-bloggers.com/getting-data-from-an-online-source/
+# https://theodi.org/article/how-to-use-r-to-access-data-on-the-web/
+# https://www.dezyre.com/data-science-in-r-programming-tutorial/r-tutorial-importing-data-from-web
 
 # write.csv(MyData, file = "MyData.csv",row.names=FALSE)
 # write.csv(MyData, file = "MyData.csv",row.names=FALSE, na="")
