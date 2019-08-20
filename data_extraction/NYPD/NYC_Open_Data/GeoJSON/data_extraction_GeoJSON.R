7598b8fec5c17924654ccac591f2d240d1ebb49f
MCTTAN <mariechantaltan@gmail.com>
2019-08-20 04:19:13 -0400

    commit

--- data_extraction/NYPD/NYC_Open_Data/GeoJSON/data_extraction_GeoJSON.R
+++ data_extraction/NYPD/NYC_Open_Data/GeoJSON/data_extraction_GeoJSON.R
@@ -19,16 +19,16 @@ devtools::install_github("ropensci/geojsonio")
 setwd("~/virtulis/data_extraction/NYPD/NYC_Open_Data/GeoJSON")
 
 u <- "https://data.cityofnewyork.us/resource/h9gi-nx95.geojson"
-downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYC_Open_Data/GeoJSON/motor_vehicle_collisions.GeoJSON")
+downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYPD/NYC_Open_Data/GeoJSON/motor_vehicle_collisions.GeoJSON")
 # nyc_open_data <- read.table("https://data.cityofnewyork.us/resource/h9gi-nx95.geojson")
 # nyc_open_data
 
 u <- "https://data.cityofnewyork.us/resource/qgea-i56i.geojson"
-downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYC_Open_Data/GeoJSON/complaint_data_historic.GeoJSON")
+downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYPD/NYC_Open_Data/GeoJSON/complaint_data_historic.GeoJSON")
 
 u <- "https://data.cityofnewyork.us/resource/5uac-w243.geojson"
-downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYC_Open_Data/GeoJSON/complaint_data_current_year_to_date.GeoJSON")
+downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYPD/NYC_Open_Data/GeoJSON/complaint_data_current_year_to_date.GeoJSON")
 
 u <- "https://data.cityofnewyork.us/resource/h9gi-nx95.geojson"
-downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYC_Open_Data/GeoJSON/motor_vehicle_collisions_summary.GeoJSON")
+downloader::download(url = u, destfile = "~/virtulis/data_extraction/NYPD/NYC_Open_Data/GeoJSON/motor_vehicle_collisions_summary.GeoJSON")
 
