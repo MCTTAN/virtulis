ce06ce1ff6806a7fc3551ca48d4d93114d4e6f6d
mcttan <mariechantaltan@gmail.com>
2019-08-22 02:02:35 -0400

    commit

--- data_extraction/NYPD/NYC_Open_Data/Tables/arrest_data_year_to_date.R
+++ data_extraction/NYPD/NYC_Open_Data/Tables/arrest_data_year_to_date.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 library(lubridate)
 library(tidyr)
+library(base)
 
 ### ARREST DATA (YEAR TO DATE)
+
+# length(data) == 18
+# lengths(data) == 1000
 
 # data <- read.csv("~/virtulis/data_extraction/NYPD/NYC_Open_Data/CSV/arrest_data_year_to_date.csv")
 data <- read.csv("https://data.cityofnewyork.us/resource/uip8-fykc.csv", stringsAsFactors = FALSE)
@@ -11,8 +15,11 @@ data <- read.csv("https://data.cityofnewyork.us/resource/uip8-fykc.csv", strings
 ### Save columns into variables
 
 # data$ARREST_KEY
-
-arrest_date <- data$arrest_date # Exact date of arrest for the reported event; calendar
+arrest_dates <- format(as.Date(data$arrest_date), "%Y %d %b") # Exact date of arrest for the reported event; calendar
+arrest_weekdays <- weekdays(as.Date(data$arrest_date))
+# arrest_months <- months(as.Date(data$arrest_date))
+# data$arrest_date
+# arrest_date <- data$arrest_date
 
 # data$PD_CD
 
@@ -38,6 +45,8 @@ arrest_boro <- data$arrest_boro # Borough of arrest: B(Bronx), S(Staten Island),
 arrest_precinct <- data$arrest_precinct # Precinct where the arrest occurred; num
 
 ### Rename items
+# data <- subset(data, data$jurisdiction_code == 0 & data$jurisdiction_code == 1 & data$jurisdiction_code == 2)
+# data$jurisdiction_code <- subset(data$jurisdiction_code, data$jurisdiction_code <= 2)
 data$jurisdiction_code <- ifelse(data$jurisdiction_code == '0', 'Patrol', data$jurisdiction_code)
 data$jurisdiction_code <- ifelse(data$jurisdiction_code == '1', 'Transit', data$jurisdiction_code)
 data$jurisdiction_code <- ifelse(data$jurisdiction_code == '2', 'Housing', data$jurisdiction_code)
@@ -58,5 +67,22 @@ latitude <- data$latitude # Latitude coordinate for Global Coordinate System, WG
 
 longitude <- data$longitude # Longitude coordinate for Global Coordinate System, WGS 1984, decimal degrees (EPSG 4326); num
 
-# Output
-str(data)
+# In R, matrices are homogeneous while dataframes are heterogeneous.
+data_frame <- data.frame(arrest_dates, arrest_weekdays,
+                         law_cat_cd,
+                         arrest_boro,
+                         arrest_precinct,
+                         jurisdiction_code,
+                         age_group,
+                         perp_sex, perp_race,
+                         x_coord_cd, y_coord_cd,
+                         latitude, longitude)
+# data_frame[c("Patrol", "Transit", "Housing"), ]
+# data_frame[c('Patrol', 'Transit', 'Housing'), ]
+
+# data_frame <- subset(data_frame, (data_frame$jurisdiction_code == 'Patrol' & data_frame$jurisdiction_code == 'Transit' & data_frame$jurisdiction_code == 'Housing') )
+# data_frame
+# data_frame <- data_frame[(data_frame$jurisdiction_code == 'Patrol' & data_frame$jurisdiction_code == 'Transit' & data_frame$jurisdiction_code == 'Housing'), ]
+# data_frame <- data_frame[(data_frame$jurisdiction_code == 'Patrol' & data_frame$jurisdiction_code == 'Transit' & data_frame$jurisdiction_code == 'Housing')]
+# data_frame <- data_frame[-c(data_frame$jurisdiction_code != 'Patrol' & data_frame$jurisdiction_code != 'Transit' & data_frame$jurisdiction_code != 'Housing'), ]
+# print(data_frame)
