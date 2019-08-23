684b1d45fba1d7f5346c2a20ede76d71bf63d23d
mcttan <mariechantaltan@gmail.com>
2019-08-22 21:56:53 -0400

    commit

--- voice_control/init.js
+++ voice_control/init.js
@@ -1,207 +1,110 @@
 var initVoice = function() {
   if (annyang) {
     
-    var zoom=4;
-    //Default input values
-    Shiny.onInputChange('zip',75252);
-    Shiny.onInputChange('color','education');
+    // Default Values
+    var zoom = 4;
+    Shiny.onInputChange('zip', 19104);
+    Shiny.onInputChange('color', 'education');
     Shiny.onInputChange('size', 'income');
-    Shiny.onInputChange('top',0);
-    Shiny.onInputChange('feature',"income");
-    Shiny.onInputChange('zoom',4);
-    Shiny.onInputChange('plotx','income');
-    Shiny.onInputChange('ploty','education');
-    Shiny.onInputChange('remove',3);
-    
-    
-    //Inputs for voice command 
-    var commands = {
-      'go (to) (zip) (code) :zip': function(zip) {
-        Shiny.onInputChange('zip', zip);
-        
-      },
-      'color (by) :color': function(color) {
-        Shiny.onInputChange('color', color);
-      },
-      'size (by) :size': function(size) {
-        Shiny.onInputChange('size', size);
-      },
-      '(zoom) in': function() {
-        zoom=zoom+1;
+    Shiny.onInputChange('top', 0);
+    Shiny.onInputChange('feature', "income");
+    Shiny.onInputChange('zoom', 4);
+    Shiny.onInputChange('plotx', 'income');
+    Shiny.onInputChange('ploty', 'education');
+    Shiny.onInputChange('remove', 3);
+
+    var voice_commands = {
+      'go (to) (zip) (code) :zip': function(zip) { Shiny.onInputChange('zip', zip); },
+      'color (by) :color': function(color) { Shiny.onInputChange('color', color); },
+      'size (by) :size': function(size) { Shiny.onInputChange('size', size); },
+      '(zoom) in': function() { 
+        zoom = zoom + 1;
         Shiny.onInputChange('zoom', zoom);
       },
       '(zoom) out': function() {
-        zoom=zoom-1;
+        zoom = zoom - 1;
         Shiny.onInputChange('zoom', zoom);
       },
       'top :top (places) (by) population': function(top) {
-        
-        if(top=='one')
-        {
-          top=1;
-        }
-        else if (top=='two')
-        {
-          top=2;
-        }
-        else if (top=='three')
-        {
-          top=3;
-        }
-        
-        else if (top=='four')
-        {
-          top=4;
-        }
-        
-        else if (top=='five')
-        {
-          top=5;
-        }
-        
-        else if (top=='six')
-        {
-          top=6;
-        }
-        
-        else if (top=='seven')
-        {
-          top=7;
-        }
-        
-        else if (top=='eight')
-        {
-          top=8;
-        }
-        
-        
-        else if (top=='nine')
-        {
-          top=9;
-        }
-        else
-        {
-          
-          //Do Nothing
-          
-        }
-      
-        Shiny.onInputChange('feature','education');
-        Shiny.onInputChange('top',parseInt(top));
-        },
+        if(top == 'one') { top = 1; }
+        else if (top == 'two') { top = 2; }
+        else if (top == 'three') { top = 3; }
+        else if (top == 'four') { top = 4; }
+        else if (top == 'five') { top = 5; }
+        else if (top == 'six') { top = 6; }
+        else if (top == 'seven') { top = 7; }
+        else if (top == 'eight') { top = 8; }
+        else if (top == 'nine') { top = 9; }
+        else if (top == 'ten') { top = 10; }
+        else if (top == 'twenty') { top = 20; }
+        else if (top == 'thirty') { top = 30; }
+        else if (top == 'fourty') { top = 40; }
+        else if (top == 'fifty') { top = 50; }
+        else if (top == 'sixty') { top = 60; }
+        else if (top == 'seventy') { top = 70; }
+        else if (top == 'eighty') { top = 80; }
+        else if (top == 'ninety') { top = 90; }
+        else if (top == 'one hundred') { top = 100; }
+        else if (top == 'one thousand') { top = 1000; }
+        else { }
+        Shiny.onInputChange('feature', 'education');
+        Shiny.onInputChange('top', parseInt(top));
+      },
       'top :top (places) (by) income': function(top) {
-         if(top=='one')
-        {
-          top=1;
-        }
-        else if (top=='two')
-        {
-          top=2;
-        }
-        else if (top=='three')
-        {
-          top=3;
-        }
-        
-        else if (top=='four')
-        {
-          top=4;
-        }
-        
-        else if (top=='five')
-        {
-          top=5;
-        }
-        
-        else if (top=='six')
-        {
-          top=6;
-        }
-        
-        else if (top=='seven')
-        {
-          top=7;
-        }
-        
-        else if (top=='eight')
-        {
-          top=8;
-        }
-        
-        
-        else if (top=='nine')
-        {
-          top=9;
-        }
-        else
-        {
-         //Do Nothing
-        }
-        
-        
+        if(top == 'one') { top = 1; }
+        else if (top == 'two') { top = 2; }
+        else if (top == 'three') { top = 3; }
+        else if (top == 'four') { top = 4; }
+        else if (top == 'five') { top = 5; }
+        else if (top == 'six') { top = 6; }
+        else if (top == 'seven') { top = 7; }
+        else if (top == 'eight') { top = 8; }
+        else if (top == 'nine') { top = 9; }
+        else if (top == 'ten') { top = 10; }
+        else if (top == 'twenty') { top = 20; }
+        else if (top == 'thirty') { top = 30; }
+        else if (top == 'fourty') { top = 40; }
+        else if (top == 'fifty') { top = 50; }
+        else if (top == 'sixty') { top = 60; }
+        else if (top == 'seventy') { top = 70; }
+        else if (top == 'eighty') { top = 80; }
+        else if (top == 'ninety') { top = 90; }
+        else if (top == 'one hundred') { top = 100; }
+        else if (top == 'one thousand') { top = 1000; }
+        else { }
         Shiny.onInputChange('feature','income');
         Shiny.onInputChange('top',parseInt(top));
         },
       'top :top (places) (by) education': function(top) {
-        
-         if(top=='one')
-        {
-          top=1;
-        }
-        else if (top=='two')
-        {
-          top=2;
-        }
-        else if (top=='three')
-        {
-          top=3;
-        }
-        
-        else if (top=='four')
-        {
-          top=4;
-        }
-        
-        else if (top=='five')
-        {
-          top=5;
-        }
-        
-        else if (top=='six')
-        {
-          top=6;
-        }
-        
-        else if (top=='seven')
-        {
-          top=7;
-        }
-        
-        else if (top=='eight')
-        {
-          top=8;
-        }
-        
-        
-        else if (top=='nine')
-        {
-          top=9;
-        }
-        else
-        {
-          
-          //Do Nothing
-        }
-        Shiny.onInputChange('feature','education');
-        Shiny.onInputChange('top',parseInt(top));
+        if(top == 'one') { top = 1; }
+        else if (top == 'two') { top = 2; }
+        else if (top == 'three') { top = 3; }
+        else if (top == 'four') { top = 4; }
+        else if (top == 'five') { top = 5; }
+        else if (top == 'six') { top = 6; }
+        else if (top == 'seven') { top = 7; }
+        else if (top == 'eight') { top = 8; }
+        else if (top == 'nine') { top = 9; }
+        else if (top == 'ten') { top = 10; }
+        else if (top == 'twenty') { top = 20; }
+        else if (top == 'thirty') { top = 30; }
+        else if (top == 'fourty') { top = 40; }
+        else if (top == 'fifty') { top = 50; }
+        else if (top == 'sixty') { top = 60; }
+        else if (top == 'seventy') { top = 70; }
+        else if (top == 'eighty') { top = 80; }
+        else if (top == 'ninety') { top = 90; }
+        else if (top == 'one hundred') { top = 100; }
+        else if (top == 'one thousand') { top = 1000; }
+        else { }
+        Shiny.onInputChange('feature', 'education');
+        Shiny.onInputChange('top', parseInt(top));
         },
-      'graph :x by :y': function(x,y){
-        Shiny.onInputChange('plotx',x);
-        Shiny.onInputChange('ploty',y);
+      'graph :x by :y': function(x,y) {
+        Shiny.onInputChange('plotx', x);
+        Shiny.onInputChange('ploty', y);
         },
-      'remove': function(){
-       Shiny.onInputChange('remove',3);
-        }
+      'remove': function() { Shiny.onInputChange('remove', 3); }
     };
     
     annyang.addCommands(commands);
@@ -210,6 +113,4 @@ var initVoice = function() {
   }
 };
 
-$(function() {
-  setTimeout(initVoice, 10);
-});
+$(function() { setTimeout(initVoice, 5); });
--- voice_control/ui.R
+++ voice_control/ui.R
@@ -1,66 +1,64 @@
 library(leaflet)
 library(plotly)
 
-# Choices for drop-downs
-vars <- c(
+
+characteristics <- c(
+  # "age" = "Age"
   "education" = "education",
   "income" = "income",
   "Population" = "population"
 )
 
 
-navbarPage("virtulis", id="nav",
+navbarPage("virtulis", id = "nav",
            
            
            tabPanel("Map",
-                    div(class="outer",
+                    div(class = "outer",
                         
                         tags$head(
                           tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800"),
                           includeCSS("styles.css"),
                           includeScript("gomap.js")
                         ),
-                        # below code is for adding annyang library for Rshhiny UI
+                       
                         singleton(tags$head(
-                          tags$script(src="//cdnjs.cloudflare.com/ajax/libs/annyang/1.4.0/annyang.min.js"),
+                          tags$script(src = "//cdnjs.cloudflare.com/ajax/libs/annyang/1.4.0/annyang.min.js"),
                           includeScript('init.js')
                         )),
                         
-                        h5("Please refer 'How To Use' documentation OR Try voice commands- 'Top 10 places by Income','Go to zip code 13475','Zoom In','Color by Population','Size by income' , 'Graph Population by Income' "),
-                        #Below code is to include the leaflet maps in to UI
-                        leafletOutput("map", width="100%", height="100%"),
+                        h5("Try these voice commands: 'Top 10 places by income','Go to zip code 21117','Zoom in','Color by population','Size by income' , 'Graph population by income'"),
                         
+                        leafletOutput("map", width = "100%", height = "100%"),
                         
-                        
-                        absolutePanel(id = "controlsa", class = "panel panel-default", fixed = TRUE,
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                       draggable = TRUE, top = 100, left =20 , right = "auto", bottom = "auto",
                                       width = 400, height = "auto",
                                       
-                                      h5('Listening...'),verbatimTextOutput("default"),
+                                      # h5('Listening...'),verbatimTextOutput("default"),
+                                      h5(''), verbatimTextOutput("default"),
                                       dataTableOutput("TopN_output")
                         ),
-                        # Shiny versions prior to 0.11 should use class="modal" instead.
+                        
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                                       draggable = TRUE, top = 100, left = "auto", right = 20, bottom = "auto",
                                       width = 400, height = "auto",
                                       #tags$a(href="https://www.linkedin.com/in/abhishekmshivalingaiah", "LinkedIn    "),
-                                      #tags$a(href="https://www.linkedin.com/in/abhishekmshivalingaiah", "GitHub"),
+                                      tags$a(href="https://www.mcttan.github.io/virtulis/pages/history.html", "GitHub    "),
                                       
                                       h6("Color By"),verbatimTextOutput("color1"),
                                       h6("Size By"),verbatimTextOutput("size1"),
                                       plotlyOutput("scatterPlot")
-                                      
-                                      
-                        ),
-                        
-                        tags$div(id="cite",
-                                 'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
                         )
+                        
+                        # tags$div(id="cite",
+                        #          'Sources: ', tags$em('NYC Open Source'), ' NYPD'
+                        # )
                         
                     )
            ),
            
-           tabPanel("Data explorer",
+           tabPanel("Tabular Data",
                     fluidRow(
                       column(3,
                              selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
@@ -88,15 +86,6 @@ navbarPage("virtulis", id="nav",
                     DT::dataTableOutput("ziptable")
            ),
            
-           
-           # using iframe along with tags() within tab to display pdf with scroll, height and width could be adjusted
-           tabPanel("How To Use", 
-                    tags$iframe(style="height:700px; width:100%; scrolling=yes", 
-                                src="Talk_to_R_Documentation.pdf")
-                    
-           ),
-           
-           
-           
+           # conditionalPanel("false", icon("heartbeat"))
            conditionalPanel("false", icon("crosshair"))
 )
