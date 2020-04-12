dd745d751867667f1e735f0305e1e1dd1aa73848
rising-jsmn <ttru17@gmail.com>
2020-04-12 14:47:58 -0400

    commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    // User is signed in.
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("user-div").style.display = "initial";
+    
+  }
+  else {
+    // No user is signed in.
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("user-div").style.display = "block";
+  }
+});
+
+function login() {
+  var userEmail = document.getElementById("email-string").value;
+  var userPassword = document.getElementById("password-string").value;
+  
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    window.alert("Error: " + errorMessage);
+  });
+}
